00d62c62c0e695b6d42543d588c95837e1db820d
Chinmay Deshpande <chinmaydeshpande96@gmail.com>
2019-06-02 18:41:35 +0530

    Add custom data model.

--- QuickTodo/Controllers/TodoListViewController.swift
+++ QuickTodo/Controllers/TodoListViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["One", "Two", "Three"]
+    var itemArray = [TodoItem]()
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let newItem = TodoItem()
+        newItem.title = "One"
+        newItem.done = true
+        itemArray.append(newItem)
+        
+        let newItem2 = TodoItem()
+        newItem2.title = "Two"
+        itemArray.append(newItem2)
+        
+        let newItem3 = TodoItem()
+        newItem3.title = "Three"
+        itemArray.append(newItem3)
+        
+        let newItem4 = TodoItem()
+        newItem4.title = "Four"
+        itemArray.append(newItem4)
+        
+        /*if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            
+            itemArray = items
+        }*/
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -26,7 +50,11 @@ class TodoListViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
     }
@@ -35,14 +63,9 @@ class TodoListViewController: UITableViewController {
         
         //print("Selected item: \(itemArray[indexPath.row])")
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else {
-            
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -57,8 +80,11 @@ class TodoListViewController: UITableViewController {
             
             //print(textField.text!)
             
-            self.itemArray.append(textField.text!)
+            let newItem = TodoItem()
+            newItem.title = textField.text!
             
+            self.itemArray.append(newItem)
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
         
---
+++ QuickTodo/Data Model/TodoItem.swift
@@ -0,0 +1,15 @@
+//
+//  TodoItem.swift
+//  QuickTodo
+//
+//  Created by Chinmay Deshpande on 02/06/19.
+//  Copyright © 2019 Chinmay Deshpande. All rights reserved.
+//
+
+import Foundation
+
+class TodoItem {
+    
+    var title : String = ""
+    var done : Bool = false
+}
